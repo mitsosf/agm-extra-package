f2f779b92b90268c2a999f291c167c9b08466bab
Dimitris <dimitris@frangiadakis.com>
2019-02-05 00:07:29 +0200

    Added t&cs

--- resources/views/misc/eventTerms.blade.php
+++ resources/views/misc/eventTerms.blade.php
@@ -4,21 +4,108 @@
 
 @section('content')
     <div style="margin-top: 2%">
-        <h2><span style="font-weight: 400;">Event</span></h2>
-        <p><span style="font-weight: 400;">Version 1.0</span></p>
-        <p><span style="font-weight: 400;">The AGM Thessaloniki 2019 website located at https://event.agmthessaloniki.org (&ldquo;the Site&rdquo;) is a copyrighted work belonging to Federation of Erasmus Student Network - Greece (Hereby referred as &ldquo;OC&rdquo; or &ldquo;ESN Greece&rdquo;). Certain features of the Site may be subject to additional guidelines, terms, or rules, which will be posted on the Site in connection with such features. By accessing the Site, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing the Site. The materials contained in the Site are protected by applicable copyright and trademark law.</span></p>
-        <p><span style="font-weight: 400;">All such additional terms, guidelines, and rules are incorporated by reference into these Terms.</span></p>
-        <p><span style="font-weight: 400;">These Terms of Use described the legally binding terms and conditions that oversee your use of the Site. By logging into the Site, you are being compliant that these terms and you, represent that you have the authority and capacity to enter into these Terms.</span></p>
-        <h2><strong>Access to the Site</strong></h2>
-        <p><a href="https://www.everypay.gr/"><strong>Everypay (Payments Provider)</strong></a><strong>. </strong><span style="font-weight: 400;">"Recognizing the importance of electronic payment security, EveryPay is a licensed Payment Institution by the Bank of Greece (Decision 280/3 / 23-7-2018 Government Gazette B 3010 / 25-7-2018), and manages secure payment transactions with cards, in accordance with the regulatory framework of the card transaction security management model. Everypay is certified in accordance with the PCI DSS standard and is subject to periodic audits by a specialist advisory firm. All Everypay services are made through secure connections with 256-bit SSL certificates. EveryPay also supports the ability to use 3D Secure, an additional security service for VISA, MasterCard &amp; American Express. The payer should enter his personal code to successfully complete the transaction &raquo;</span></p>
-        <p><strong>Subject to these Terms.</strong><span style="font-weight: 400;"> The OC grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use.</span></p>
-        <p><strong>Certain Restrictions.</strong><span style="font-weight: 400;"> The rights approved to you in these Terms are subject to the following restrictions: (a) you shall not sell, rent, lease, transfer, assign, distribute, host, or otherwise commercially exploit the Site; all copyright and other proprietary notices on the Site must be retained on all copies thereof.</span></p>
-        <p><span style="font-weight: 400;">The OC reserves the right to change, suspend, or cease the Site with or without notice to you. &nbsp;You approved that OC will not be held liable to you or any third-party for any change, interruption, or termination of the Site or any part.</span></p>
-        <p><strong>Term and Termination.</strong><span style="font-weight: 400;"> Subject to this Section, these Terms will remain in full force and effect while you use the Site. &nbsp;We may suspend or terminate your rights to use the Site at any time for any reason at our sole discretion, including for any use of the Site in violation of these Terms. &nbsp;Upon termination of your rights under these Terms, your Account and right to access and use the Site will terminate immediately. You understand that any termination of your Account may involve deletion of your User Content associated with your Account from our live databases. The OC will not have any liability whatsoever to you for any termination of your rights under these Terms.</span></p>
-        <p><strong>Copyright/Trademark Information.</strong><span style="font-weight: 400;"> Copyright &copy;. All rights reserved. All trademarks, logos and service marks displayed on the Site are our property or the property of other third-parties. You are not permitted to use these Marks without our prior written consent or the consent of such third party which may own the Marks.</span></p>
-        <p><a href="https://esngreece.gr/privacy-policy"><span style="font-weight: 400;">Federation of Erasmus Student Network Greece - Privacy Policy</span></a></p>
-        <h2><span style="font-weight: 400;">Contact Information</span></h2>
-        <p><span style="font-weight: 400;">Email: agm@esngreece.gr</span></p>
+        <p><strong>Terms and conditions</strong></p>
+        <p><strong>AGM Thessaloniki 2019 - &lsquo;Extra Package&rsquo;</strong></p>
+        <p><strong>April 18th to 21th 2019 </strong></p>
+        <p>&nbsp;</p>
+        <p><span style="font-weight: 400;">In connection with the Annual General Meeting (AGM) of Erasmus Student Network (ESN) to be held in Thessaloniki, Greece from the 18th till the 21th April 2019, I understand, accept and agree that: </span></p>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">The terms and conditions of the registration process of the AGM Thessaloniki Extra Package are prescribed by the Organizing Committee (hereinafter referred to as OC), which reserves the right to cancel the participant's registration or exclude the participant from the meeting if the conditions aren&rsquo;t met. &nbsp;</span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">The payment of the fee formalises the registration for the event and grants access to all the scheduled activities in the terms communicated in the application form and sent by e-mail. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">The amount of the fee is ONE HUNDRED SIXTY Euros (160&euro;). Participants will pay the fee via credit/debit card (Visa, MasterCard or Maestro) using the payments system the OC provides. Additionally, a </span><strong>TWENTY EURO </strong><span style="font-weight: 400;">(</span><strong>20&euro;) deposit will be collected by the OC shortly before or during the check-in. The 20&euro; deposit is refundable, in case no damages were caused by the participant during the event. </strong><span style="font-weight: 400;">Participants may be able to pay the deposit beforehand via credit/debit card (Visa, MasterCard or Maestro), using the Deposits payment system the OC may provide.</span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">The registrations shall open on 18th February 2019 and shall close on 22th February 2019 at 23:59 CET. After applying to the event through the registration form, the participant shall proceed with the payment as soon as possible. Simply registering to the event does not guarantee a spot to the wannabe participant. The registrations &amp; payments flow works under the principle of &lsquo;first come first served&rsquo;. Payment approvals are automatic and there is no need to upload a proof of payment. If the spots are full, or the fee is not paid within the aforementioned deadline, the registration will be cancelled. The deadline for the voluntary cancellation of a participant is on 3rd March 2019 at 23:59 CET; from that moment on, the participation fee is not refundable. However, with the permission of the OC the place may be transferred to a third party, who shall then be subject to these Terms and Conditions. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">Participants should respect and follow the rules and guidelines given to them by the OC. The OC reserves the right of expulsion from the event (before the event or during its execution) of any participant whose conduct is considered irresponsible, disrespectful and reprehensible, according to the opinion of the OC. In case of expulsion, the participant will lose the right to participate in all activities as well as the right of payment refund. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">The OC reserves the right to amend or modify any part of the program if necessary at any time. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">In case that any damage occurs in a specific room, the deposit under term 3 shall be forfeited and the guests of the room and/or the accompanying persons of theirs, shall be liable to refund the OC with any extra amount till the full amount of damage is met. If any damage occurs in the corridor of the hotel or any other common part of the hotel, the participants staying in the corridor shall be liable to refund to the OC the full amount of the damage made in the corridor. Extra costs such as damages of furniture, extra cleaning, stickers on the furniture, walls, floors or anywhere that is considered as hotel property, unauthorized usage of fire extinguisher, etc. however charged by the hotel to the OC, will be considered as a damage and treated as mentioned here above. You may be required to leave a deposit for your room key which will be forfeit if your key is not returned.</span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">In case that any damage occurs by the participant in a private bus the OC provides, the passenger shall be liable to refund to the OC the full amount of the damage made in the bus.</span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">The OC disclaims any liability for damages of any nature caused by delegates or accompanying persons, or loss of or damage to their personal property as a result of the event. All damages or losses made by a participant in the hotels and/or at any other venue (e.g. official plenary rooms, clubs, bars, restaurants, public facilities, etc.) shall remain the participant&rsquo;s responsibility. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">A duly authorized member of the OC and of the International Board is entitled to expel any participant from the meeting if committing any kind of crime, unauthorized drug usage, misbehaving towards the participants, the OC or local inhabitants or any third person during the AGM. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">The OC cannot be responsible for healthcare, dental and ambulance services during the event. The OC strongly recommends that participants make all necessary travel and/or medical insurance arrangements, which should cover the possibility of flight cancellation due to strikes and other causes and medical expenses in the host country. Therefore, the OC and its local co-organizers shall not accept any responsibility of any nature for personal injury, death, loss or damage to property or participants however caused or arising. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">Smoking in all indoor public places (including hotel rooms, lobbies, institutions, universities, bars, restaurants etc.) in Greece is forbidden. Violation of this law will result a </span><strong>fine up to &euro;600 </strong><span style="font-weight: 400;">and may result in the exclusion of the participant. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">Participants cannot take any food or drink to the hotel rooms (except for what is necessary for the International Dinner). </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">Participants shall respect the transport schedule established by the OC. The OC is not responsible for participants&rsquo; transport beyond the official programme. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">Participants hereby grant permission to Erasmus Student Network AISBL, its officers and trustees, employees, agents, representatives and licensees (hereafter all referred to as ESN AISBL), to photograph or videotape in any media, their image, likeness or depiction as well as the sound of their voice without payment, rights to royalties or any other consideration. Participants understand and agree that their image may be edited, cropped, retouched, copied, exhibited, published or distributed and they waive any right to inspect or approve the finished product wherein participant&rsquo;s likeness appears. Additionally, participants waive any right to royalties or other compensation arising or related to the use of their image or recording. Participants hereby consent to and permit all this material of them to be used for any legal purpose and in any medium, including print or electronic, in diverse educational and promotional settings within an unrestricted geographic area. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <p><span style="font-weight: 400;">Photographic, audio or video recordings may be used indicatively for the following purposes: &nbsp;</span></p>
+        <p>&nbsp;</p>
+        <ul>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">conference presentations</span></li>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">educational presentations or courses</span></li>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">informal presentations</span></li>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">on-line educational courses</span></li>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">educational and promotional videos </span></li>
+        </ul>
+        <p>&nbsp;</p>
+        <p><span style="font-weight: 400;">Participants understand this permission signifies that photographic, audio or video recordings of them may be electronically displayed via the Internet or in a public educational setting, with or without associating names thereto. </span></p>
+        <p>&nbsp;</p>
+        <p><span style="font-weight: 400;">Participants hereby grant to ESN AISBL all rights to reproduce, display, and disseminate photographs, images, audio and video of me, in whole or in part, or altered in character or form, as well as related posters, presentations, programs, and publications. Participants further grant to ESN AISBL all rights to make reproductions of such photographs, images, audio and video through any media, for educational purposes, art, entertainment, advertising, internal use, or other lawful purposes. Participants understand that they will be consulted about the use of photographs, images, audio and/or video of them for commercial purposes. There is no time limit on the validity of this release nor is there any geographic limitation on where these materials may be distributed. Participants hereby fully and forever discharge and release ESN AISBL its officers and trustees, employees, agents, representatives and licensees from any claim for damages or claims of any kind (including but not limited to invasion of privacy, defamation, false light or misappropriation of name, likeness or image) or any other cause of action arising out of the use or publication, distribution, modification and exhibition of photographs, images, audio and/or video of them by ESN AISBL and agree not to sue or otherwise initiate legal proceedings against ESN AISBL for such use or publication. </span></p>
+        <p>&nbsp;</p>
+        <p><span style="font-weight: 400;">This release applies to photographic, image, audio or video recordings collected as part of the sessions related to the original event description, as well as the event itself as a whole. </span></p>
+        <p>&nbsp;</p>
+        <p><span style="font-weight: 400;">Should the Participants wish to withdraw the aforementioned permission (in section 14), this has to be done in writing by sending an email to </span><span style="font-weight: 400;">secretariat@esn.org</span><span style="font-weight: 400;"> no later than the commencement of the event. </span></p>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">For the purposes of complying with the legal requirements for the protection of personal data and in accordance with Data Protection Regulation, the information provided in this form will only be used by ESN International or duly authorised Organising Committee of the AGM Thessaloniki for the purposes of an efficient organisation and management of the event AGM Thessaloniki and will not be disclosed to any other party for any other purpose, without the permission of the participant.</span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <p><span style="font-weight: 400;">All information acquired by ESN International or duly authorised Organising Committee of the AGM Thessaloniki will be treated as confidential.</span></p>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">All the participants shall respect the </span><a href="https://wiki.esn.org/_media/downloads/code_of_conduct.pdf"><strong>Code of Conduct</strong></a><span style="font-weight: 400;"> as approved by AGM Germany 2017.</span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">In addition to the terms and conditions above, participants have to respect the laws and regulations of Greece. Any violation may result in civil and/or criminal charge, prosecution, and penalties and/or exile according to the national law. </span></li>
+        </ol>
+        <p>&nbsp;</p>
+        <ol>
+            <li style="font-weight: 400;"><span style="font-weight: 400;">These Terms and Conditions and any necessary enforcement thereof shall be subject to the jurisdiction of the law of Greece. Agreeing to these Terms and Conditions does not affect your statutory rights. </span></li>
+        </ol>
+        <p><br /><br /></p>
     </div>
 
 
